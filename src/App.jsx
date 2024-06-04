60432f3af1053a56f98828c98f772ecda2981468
shencanet <santi.dionis@gmail.com>
2024-06-04 22:02:27 +0200

    ADD: app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
  export const App = () => { // exportaciones nombradas de la funcion App que es un componente de react que se encarga de renderizar el contenido en el navegador web
   return (
     <div>
-      <h1>My App Hola Soy React</h1>
+      <h1 className= "text-center">My App Hola Soy React</h1>
       <p>Welcome to my app</p>
     </div>
   );
