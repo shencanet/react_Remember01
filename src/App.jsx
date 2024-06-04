c0a8d582f7168506c61a0a079721edecc863a3d6
shencanet <santi.dionis@gmail.com>
2024-06-04 21:46:20 +0200

    ADD: app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1>My App Hola Soy React</h1>
+      <p>Welcome to my app</p>
+    </div>
+  );
+};
+export default App;//exportacion por defecto de la funcion App que es un componente de react que se encarga de renderizar el contenido en el navegador web 
