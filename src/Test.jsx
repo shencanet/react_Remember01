68be4a07e494721595128ca0be388c9bdb543400
shencanet <santi.dionis@gmail.com>
2024-06-05 20:55:11 +0200

    ADD: app.jsx

---
+++ src/Test.jsx
@@ -0,0 +1,32 @@
+const Shen = () => {
+    const title1 = "titulo 1";
+    const title2 = "titulo 2";
+
+    const ClassColors = {
+        primary: "text-primary",
+        info: "text-info",
+        danger: "text-danger",
+        warning: "text-warning",
+        dark: "text-dark",
+
+    };
+    const classTitle = 
+    {
+        primary: "text-primary",
+        info: "text-info",
+        danger: "text-danger",
+        warning: "text-warning",
+        dark: "text-dark",
+    }
+    const pathImg = "https://media.giphy.com/media/3o7TKz9bX9v6hZ8NSA/giphy.gif";
+    const color = ClassColors.dark;
+    return (
+        <>
+            <h1 className={`${ClassColors.primary}`}>{title1}</h1>
+            <h1 className={`${classTitle} ${warning}`}>{title2}</h1>
+            <img src={pathImg} alt={`imagen-${title2}`} />
+        </>
+    );
+    }
+
+    export default Shen;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'//importacion por defecto
 import { App } from './App.jsx'//importacion nombrada entre llaves
+import Shen from './Test.jsx'
 import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
+
+    <Shen />
     <App />
+
+
+
   </React.StrictMode>,
 )
