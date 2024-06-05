ce7c0e8f8f136c1d558a1b213786e116b302f889
shencanet <santi.dionis@gmail.com>
2024-06-05 19:46:40 +0200

    ADD: app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
- export const App = () => { // exportaciones nombradas de la funcion App que es un componente de react que se encarga de renderizar el contenido en el navegador web
+ 
+ 
+ export const App = () => { 
+    const title = 'My App Hola Soy React arriba ';
+    const classTitle = 'text-center';
+    const pathImg = 'https://media.giphy.com/media/3o7TKz9bX9v6hZ8NSA/giphy.gif';
+ 
   return (
-    <div>
-      <h1 className= "text-center">My App Hola Soy React</h1>
-      <p>Welcome to my app</p>
-    </div>
+    
+    <>//Fragment de react que se encarga de renderizar varios elementos hijos sin necesidad de un div padre
+
+      <h1 className={classTitle}>My App Hola Soy React</h1>
+      <h2>{title}</h2>
+      <img src={pathImg} alt="Gif" />
+    
+    </>
   );
 };
 //export default App;//exportacion por defecto de la funcion App que es un componente de react que se encarga de renderizar el contenido en el navegador web 
