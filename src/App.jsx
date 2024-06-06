c81e32f42f4c05bdf50764c204b4bd20ed5b072b
shencanet <santi.dionis@gmail.com>
2024-06-06 21:02:26 +0200

    ADD: app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// Define MyButton component
+// Define MyButton component windows punto emogis
 const MyButton = () => {
   return (
   <button>Click me!</button>
@@ -9,6 +9,9 @@ const App = () => {
     const title = 'My App Hola Soy React arriba ';
     const classTitle = 'text-center';
     const pathImg = 'https://media.giphy.com/media/3o7TKz9bX9v6hZ8NSA/giphy.gif';
+
+    const Fruits = [ '💋', '✌', '✔', '👍', '👌', '👏', '👀', '👅', '👄', '👂', '👃', '👆', '👇', '👈', '👉', '👊', '👋', '👎']
+
  
   return (
     
@@ -17,7 +20,13 @@ const App = () => {
       <h1 className={classTitle}>My App Hola Soy React</h1>
       <h2>{title}</h2>
       <img src={pathImg} alt={`imagen-${title}`} />
+
       <MyButton />
+      <ul>
+        {Fruits.map((fruit, index) => (
+          <li key={index}>{fruit}</li>
+        ))}
+        </ul>
     
     </>
   );
