a21e2e9b194acad2c2c25d8f333550b1429ad57f
shencanet <santi.dionis@gmail.com>
2024-06-07 21:36:51 +0200

    ADD: app.jsx

--- src/Test.jsx
+++ src/Test.jsx
@@ -13,6 +13,18 @@ const Shen = () => {
 
     };
 
+    const WelcomeText = ({user}) => {
+
+        if(user){
+            return <h1 className={`${ClassColors.info}`}>Hola Usuario</h1>
+        }else{
+            return <h1 className={`${ClassColors.danger}`}>No hay usuario</h1>
+
+        }
+    
+      
+    }
+
     const pathImg = "https://media.giphy.com/media/3o7TKz9bX9v6hZ8NSA/giphy.gif";
     const color = ClassColors.dark;
     return (
@@ -20,6 +32,7 @@ const Shen = () => {
             <h1 className={`${ClassColors.primary}`}>{title1}</h1>
 
             <img src={pathImg} alt={`imagen-${title2}`} />
+            <WelcomeText user={user} />
             {
                 user ? <h1 className={`${ClassColors.info}`}>Hola Usuario</h1> : <h1 className={`${ClassColors.danger}`}>No hay usuario</h1>
             }
