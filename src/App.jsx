1e3f600037220c9044fe4be47e69b19feb83f43c
shencanet <santi.dionis@gmail.com>
2024-06-08 20:47:26 +0200

    ADD: COMMENTS

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-// Define MyButton component with destructuring of props to get text
+// Componente MyButton para renderizar un botón con el texto proporcionado
 const MyButton = ({ text }) => {
+  // Registrando el texto del botón en la consola para depuración
   console.log(text);
-  return (
-    <button>{text}</button>
-  );
+  return <button>{text}</button>;
 };
 
-// Define ItemFruit component with destructuring of props to get fruit
+// Prop types para el componente MyButton para la comprobación de tipos
+MyButton.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+// Componente ItemFruit para renderizar un elemento de lista con el emoji de fruta o texto proporcionado
 const ItemFruit = ({ fruit }) => {
-  return (
-    <li>{fruit}</li>
-  );
+  return <li>{fruit}</li>;
 };
 
-// Main App component
-const App = () => {
-  const title = 'My App Hola Soy React arriba';
-  const classTitle = 'text-center';
-  const pathImg = 'https://media.giphy.com/media/3o7TKz9bX9v6hZ8NSA/giphy.gif';
+// Prop types para el componente ItemFruit para la comprobación de tipos
+ItemFruit.propTypes = {
+  fruit: PropTypes.string.isRequired,
+};
 
-  // Array of fruits emojis
+// Componente principal App
+const App = () => {
+  const title = 'Mi Aplicación Hola Soy React arriba'; // Título para la aplicación
+  const classTitle = 'text-center'; // Nombre de clase para el estilo del título
+  const pathImg = 'https://media.giphy.com/media/3o7TKz9bX9v6hZ8NSA/giphy.gif'; // Ruta a una imagen
+
+  // Array de emojis de frutas para ser mostrados como elementos de lista
   const fruits = ['💋', '✌', '✔', '👍', '👌', '👏', '👀', '👅', '👄', '👂', '👃', '👆', '👇', '👈', '👉', '👊', '👋', '👎'];
 
   return (
     <>
-      <h1 className={classTitle}>{title}</h1>
-      <img src={pathImg} alt={`imagen-${title}`} />
+      <h1 className={classTitle}>{title}</h1> {/* Mostrando el título con alineación de texto al centro */}
+      <img src={pathImg} alt={`imagen-${title}`} /> {/* Mostrando una imagen con texto alternativo */}
 
-      <MyButton text='Boton 1' />
-      <MyButton text='Boton 2' />
-      <MyButton text='Boton 3' />
+      {/* Renderizando tres componentes MyButton con textos diferentes */}
+      <MyButton text='Botón 1' />
+      <MyButton text='Botón 2' />
+      <MyButton text='Botón 3' />
 
+      {/* Renderizando una lista de frutas usando el componente ItemFruit */}
       <ul>
         {fruits.map((fruit, index) => (
+          // Usar el índice como clave para los elementos de la lista, lo cual generalmente se desaconseja en casos donde la lista puede cambiar
           <ItemFruit key={index} fruit={fruit} />
         ))}
       </ul>
@@ -42,4 +53,4 @@ const App = () => {
   );
 };
 
-export default App; // Export App component as default export
+export default App; // Exportando el componente App para su uso en otras partes de la aplicación
--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <MyBotton />
 
 
-
-
   </React.StrictMode>,
 )
