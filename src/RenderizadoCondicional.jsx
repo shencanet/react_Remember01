1a49695b3015c349e07f25c6b48c3a0bf633ec08
shencanet <santi.dionis@gmail.com>
2024-06-08 20:42:09 +0200

    ADD: COMMENTS

--- src/RenderizadoCondicional.jsx
+++ src/RenderizadoCondicional.jsx
@@ -1,17 +1,27 @@
-
-
 import PropTypes from 'prop-types';
-const MyBotton = (props) => {
+
+// Definición del componente funcional MyButton
+const MyButton = (props) => {
+  // Destructuración de props para obtener las propiedades directamente
+  const { text, edad } = props;
+
+  // Registro en consola de las props para depuración
   console.log(props);
+
+  // Retorno del botón con las propiedades aplicadas correctamente
+  // Se corrige el uso de 'text' y 'edad' para que sean parte del contenido del botón, no propiedades del elemento <button>
   return (
-    <button text="text" edad={10}>aquii</button>
+    <button>
+      {text}, Edad: {edad}
+    </button>
   );
 }
 
-MyBotton.propTypes = {
-    text: PropTypes.string.isRequired,
-    edad: PropTypes.number.isRequired,
-    };
+// Definición de propTypes para validar las props recibidas por el componente
+MyButton.propTypes = {
+  text: PropTypes.string.isRequired, // 'text' debe ser un string y es requerido
+  edad: PropTypes.number.isRequired, // 'edad' debe ser un número y es requerido
+};
 
-export default MyBotton;
+export default MyButton; // Exportación del componente para su uso en otros archivos
 
